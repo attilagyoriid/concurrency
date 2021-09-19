edf8a5d4b9c3c156fea0542353b2d57ba1b64eb4
Attila Gyori <attila.gyori.id@gmail.com>
2021-09-19 17:08:51 +0200

    Performance assert

--- src/test/java/com/ag/concurrency/main/forkjoin/FindMaximumParallelTest.java
+++ src/test/java/com/ag/concurrency/main/forkjoin/FindMaximumParallelTest.java
@@ -2,25 +2,33 @@ package com.ag.concurrency.main.forkjoin;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.Benchmark;
 
 import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
+
 
 public class FindMaximumParallelTest {
 
 
     @Test
     public void parallelComputation() {
+        int expectedPerformanceInMillis = 900;
         long[] longs = generateRandomIntArray();
         long expectedMax = Arrays.stream(longs).max().orElse(0);
 
         ForkJoinPool forkJoinPool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
         FindMaximumParallel findTask = new FindMaximumParallel(longs, 0, longs.length);
+
+        long start = System.currentTimeMillis();
         Long actualMax = forkJoinPool.invoke(findTask);
+        long performanceInMillis = System.currentTimeMillis() - start;
+        System.out.println("Time taken: " + performanceInMillis + "ms");
 
         System.out.println("Maximum found: " + actualMax);
         Assertions.assertThat(actualMax).isEqualTo(expectedMax);
+        Assertions.assertThat(performanceInMillis < expectedPerformanceInMillis).isTrue().withFailMessage("Performance is slow than " + performanceInMillis + "ms");
 
     }
 
